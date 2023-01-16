4ca39a1c023d424dfdd8c59cf241529534afe074
Niclas Haderer <niclas.haderer.dev@gmail.com>
2023-01-16 18:09:22 +0100

    First draft for the database

--- notes-app/deploy.sh
+++ notes-app/deploy.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env zsh
 
-kubectl create secret generic pg-user-password --from-literal=password=mysecretpassword
-helm install my_notes .
+NAMESPACE="default"
+
+minikube kubectl -- delete secret pg-user-password --namespace $NAMESPACE
+minikube kubectl -- create secret generic pg-user-password --from-literal=user-password="mysecretpassword" --namespace $NAMESPACE
+helm install my-notes . --namespace $NAMESPACE
+sleep 10
+minikube kubectl -- port-forward my-notes-postgresql-0 5432:5432
+# Run the following command to check if we can access the container
+# psql -h 127.0.0.1 -p 5432 -d notes -U u_notes -W
