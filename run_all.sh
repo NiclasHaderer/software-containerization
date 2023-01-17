36840709e32763228c0c5a16ff4055aed92a74b1
andfaxle <paul.riedlingen@gmail.com>
2023-01-17 10:47:22 +0100

    added first draft of backend-deployment

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,10 @@
 docker image rm containerization-frontend
 docker build frontend -t containerization-frontend
 
-docker image rm containerization-backend
-docker build backend -t containerization-backend
+docker image rm gcr.io/containerisation/containerization-backend:latest
+docker build backend -t gcr.io/containerisation/containerization-backend:latest
+docker push gcr.io/containerisation/containerization-backend:latest
 
 docker compose up
+
+# See project in gcloud console: https://console.cloud.google.com/gcr/images/containerisation
