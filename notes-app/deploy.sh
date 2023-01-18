f526b941007c47a32f7425e594f35510f1a11019
Niclas Haderer <niclas.haderer.dev@gmail.com>
2023-01-18 12:18:42 +0100

    Renamed stuff

--- notes-app/deploy.sh
+++ notes-app/deploy.sh
@@ -3,7 +3,7 @@
 NAMESPACE="default"
 
 helm delete my-notes --namespace $NAMESPACE
-sleep 1
+sleep 3
 minikube kubectl -- delete secret pg-user-password --namespace $NAMESPACE
 minikube kubectl -- create secret generic pg-user-password --from-literal=user-password="super_secret" --namespace $NAMESPACE
 helm install my-notes . --namespace $NAMESPACE
