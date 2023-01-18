2c41873fcae7ff3b3883860fa037cd03ee2888bc
Niclas Haderer <niclas.haderer.dev@gmail.com>
2023-01-18 13:25:39 +0100

    Added more configuration options

--- backend/todo_api/settings.py
+++ backend/todo_api/settings.py
@@ -8,14 +8,15 @@ class _Settings(BaseSettings):
     db_user = Field("todo_user", env="DB_USER")
     db_password = Field("super_secret", env="DB_PASSWORD")
     db_driver = Field("postgresql+aiopg", env="DB_DRIVER")
-    db_uri = Field("127.0.0.1:5432", env="DB_URI")
+    db_host = Field("127.0.0.1", env="DB_HOST")
+    db_port = Field("5432", env="DB_PORT")
     db_name = Field("notes", env="DB_NAME")
     single_node = Field(True, env="SINGLE_NODE")
 
     @property
     def db_connection(self) -> str:
         return (
-            f"{self.db_driver}://{self.db_user}:{self.db_password}@{self.db_uri}/{self.db_name}"
+            f"{self.db_driver}://{self.db_user}:{self.db_password}@{self.db_host}/{self.db_name}"
         )
 
 
