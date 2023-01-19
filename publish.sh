3f66120f7f900df7c1677e41313c2032c4904844
Niclas Haderer <niclas.haderer.dev@gmail.com>
2023-01-19 19:09:22 +0100

    Fixed some stuff connected with gcloud

--- publish.sh
+++ publish.sh
@@ -1,10 +1,10 @@
-docker image rm containerization-frontend
-docker build frontend -t containerization-frontend
+docker image rm gcr.io/containerisation/containerization-frontend
+docker build frontend -t gcr.io/containerisation/containerization-frontend:latest
+docker push gcr.io/containerisation/containerization-frontend:latest
 
 docker image rm gcr.io/containerisation/containerization-backend:latest
 docker build backend -t gcr.io/containerisation/containerization-backend:latest
 docker push gcr.io/containerisation/containerization-backend:latest
 
-docker compose up
 
 # See project in gcloud console: https://console.cloud.google.com/gcr/images/containerisation
