3b5f7e919b70af84df3332ea1afa01ac3823141f
Niclas Haderer <niclas.haderer.dev@gmail.com>
2023-01-23 11:32:57 +0100

    Improved the cloud deployment script

--- helm/deploy_cloud.sh
+++ helm/deploy_cloud.sh
@@ -1,40 +1,77 @@
 #!/bin/bash
 
-# Deploys the notes-app to a cloud kubernetes cluster hostet
-# on Google Cloud Platform using helm.
-
-# !! This will cost money, running the smallest cluster costs
-# !! about 200$ per month.
-
-# !! before running this script, make sure the glcoud cli is
-# !! authenticated and the correct project is selected
-
-# !! make sure kubectl is installed and configured to connect
-# !! to the correct cluster
-
-# !! also, make sure the docker images are built and pushed to
-# !! the container registry.
-
-# read therefore: https://www.notion.so/Google-Cloud-d1b0e69eb7434504b45256123ab51854
-
-# 1) Install NGINX Ingress Controller on GKE according to:
-# https://medium.com/@glen.yu/nginx-ingress-or-gke-ingress-d87dd9db504c
-
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm repo update
-# todo: delete the ingress controller before installing it again to avoid errors
-helm install nginx-ingress ingress-nginx/ingress-nginx --set controller.service.loadBalancerIP=34.160.138.104
-
-NAMESPACE="default"
+# The kubectl context
 CONTEXT="gke_containerisation_europe-west4-a_containerization-cluster-v2"
 
-# 2) Apply the db.secret.yaml file to the cluster using kubectl
+# Default values
+first_time=false
+namespace=default
+upgrade=false
+reinstall=false
 
-kubectl delete secret pg-user-password --namespace $NAMESPACE --context $CONTEXT
-kubectl apply -f db.secret.yaml --namespace $NAMESPACE --context $CONTEXT
+TEMP=$(getopt -o '' --long first_time:,namespace:,upgrade,reinstall -n 'deploy_could.sh' -- "$@")
+eval set -- "$TEMP"
 
-# 3) Install app using helm
-helm uninstall my-notes --namespace $NAMESPACE --kube-context $CONTEXT --wait
-echo "Waiting for helm to delete the notes-app ..."
-sleep 10
-helm install my-notes . --namespace $NAMESPACE  --kube-context $CONTEXT
+while true; do
+  case "$1" in
+    --first_time)
+      first_time=true
+      shift 1;;
+    --namespace)
+      namespace="$2"
+      shift 2;;
+    --upgrade)
+      upgrade=true
+      shift;;
+    --reinstall)
+      reinstall=true
+      shift;;
+    -h|--help)
+      echo "Usage: deploy_could.sh [--first_time] [--namespace=string] [--upgrade] [--reinstall]"
+      exit 0;;
+    --)
+      shift
+      break;;
+    *)
+      echo "Internal error!" >&2
+      exit 1;;
+  esac
+done
+
+if ! $upgrade && ! $reinstall; then
+    echo "Either upgrade or reinstall must be specified." >&2
+    exit 1
+fi
+
+if $upgrade && $reinstall; then
+    echo "Only one of upgrade or reinstall can be specified." >&2
+    exit 1
+fi
+
+if $first_time; then
+    # 1) Install the ingress controller
+    helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+    helm repo update
+    helm install nginx-ingress ingress-nginx/ingress-nginx --set controller.service.loadBalancerIP=34.160.138.104
+fi
+
+if $upgrade; then
+    # 2) Update the database secret
+    kubectl delete secret pg-user-password --namespace "$namespace" --context $CONTEXT
+    kubectl apply -f db.secret.yaml --namespace "$namespace" --context $CONTEXT
+
+    # 3) Upgrade the app using helm
+    helm upgrade my-notes . --namespace "$namespace" --kube-context $CONTEXT
+fi
+
+if $reinstall; then
+    # 2) Update the database secret
+    kubectl delete secret pg-user-password --namespace "$namespace" --context $CONTEXT
+    kubectl apply -f db.secret.yaml --namespace "$namespace" --context $CONTEXT
+
+    # 3) Reinstall the app using helm
+    helm uninstall my-notes --namespace "$namespace" --kube-context $CONTEXT --wait
+    echo "Waiting for helm to delete the notes-app ..."
+    sleep 10
+    helm install my-notes . --namespace "$namespace"  --kube-context $CONTEXT
+fi
