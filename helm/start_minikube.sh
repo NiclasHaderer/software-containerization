9f156dfb2d9db1079e7d0bd331d5940dfe814219
Niclas Haderer <niclas.haderer.dev@gmail.com>
2023-01-23 12:00:40 +0100

    Fixed local ingress

--- helm/start_minikube.sh
+++ helm/start_minikube.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-minikube start --network-plugin=cni --cni=calico
+minikube start --network-plugin=cni --cni=calico --kubernetes-version=v1.24.3
 minikube addons enable ingress
 minikube addons enable dashboard
 
