a0ac07752fffd2a83519eb3233385453f732939e
Niclas Haderer <niclas.haderer.dev@gmail.com>
2023-01-24 16:51:22 +0100

    Set replicas to 3

--- helm/start_minikube.sh
+++ helm/start_minikube.sh
@@ -2,5 +2,6 @@
 minikube start --network-plugin=cni --cni=calico --kubernetes-version=v1.24.3
 minikube addons enable ingress
 minikube addons enable dashboard
+minikube addons enable metrics-server
 
 
